1d0869f2e63f5c279f85f2124a91139b99dbf580
George Alonge <georgedelson@gmail.com>
2017-01-08 22:24:45 +0100

    implement actual error reporting

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,14 +11,14 @@ Template.postSubmit.events({
 		Meteor.call('postInsert', post, function(error, result) {
 
 			//display the error to the user and abort
-			
+
 			if(error)
-				return alert(error.reason)
+				throwError(error.reason);
 
 			//show this result but route anyway
 
 			if(result.postExists)
-				alert('This link has already been posted');
+				throwError("This link has already been posted");
 
 			Router.go('postPage', {_id: result._id});
 
