9b599c4114c63b3ad57266001b53d30702af0e16
George Alonge <georgedelson@gmail.com>
2017-01-08 19:23:48 +0100

    enforce post url uniqueness

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -9,11 +9,14 @@ Template.postSubmit.events({
 		};
 
 		Meteor.call('postInsert', post, function(error, result) {
+
 			//display the error to the user and abort
-			if(error) 
+			
+			if(error)
 				return alert(error.reason)
 
 			//show this result but route anyway
+
 			if(result.postExists)
 				alert('This link has already been posted');
 
