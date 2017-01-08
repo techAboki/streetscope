3bd11b51f603739972cacc76def62f4fbb5d721e
George Alonge <georgedelson@gmail.com>
2017-01-08 12:39:59 +0100

    remove insecure, setup new posts and allow certain writes to posts

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -3,13 +3,13 @@ Template.postSubmit.events({
 		e.preventDefault();
 
 		var post = {
-			url: $(e.target).find('[name=url').val(),
+			url: $(e.target).find('[name=url]').val(),
 			title: $(e.target).find('[name=title]').val()
 				
 		};
 
 		post._id = Posts.insert(post);
-		Router.go('postPage'.post);
+		Router.go('postPage', post);
 	}
 
 });
