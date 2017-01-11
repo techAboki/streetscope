d7eb2d063347ef690301b775955bbb60b0370082
George Alonge <georgedelson@gmail.com>
2017-01-11 11:12:02 +0100

    grey out upvote link when not signed in/ already voted

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -8,6 +8,14 @@ Template.postItem.helpers({
 		var a = document.createElement('a');
 		a.href = this.url;
 		return a.hostname;
+	},
+	upvotedClass: function() {
+		var userId = Meteor.userId();
+		if(userId && !_.include(this.upvoters, userId)) {
+			return 'btn-primary upvotable';
+		} else {
+			return 'disabled';
+		}
 	}
 	// commentsCount: function() {
 	// 	return Comments.find({postId: this._id}).count();
@@ -15,7 +23,7 @@ Template.postItem.helpers({
 });
 
 Template.postItem.events({
-	'click .upvote': function(e) {
+	'click .upvotable': function(e) {
 		e.preventDefault();
 		Meteor.call('upvote', this._id);
 	}
