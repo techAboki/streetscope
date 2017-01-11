428ad1da6a32543a5e9bf8f62fadc1d345739116
George Alonge <georgedelson@gmail.com>
2017-01-11 16:10:56 +0100

    fade items in when drawn

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -2,6 +2,12 @@
 Template.postsList.onRendered(function ()
 	 { 
 		this.find('.wrapper')._uihooks = {
+			insertElement: function(node, next) {
+				$(node)
+				.hide()
+				.insertBefore(next)
+				.fadeIn();
+			},
 
 			moveElement: function (node, next) {
 				 var $node = $(node), $next = $(next); 
@@ -25,5 +31,13 @@ Template.postsList.onRendered(function ()
 			      // reset everything to 0, animated
 			      $node.addClass('animate').css('top', 0);
 			      $inBetween.addClass('animate').css('top', 0);
-			    }
-} });
+			    },
+
+			    removeElement: function(node) {
+				
+					$(node).fadeOut(function() {
+						$(this).remove();
+					});
+				}
+			} 
+		});
